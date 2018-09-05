7d8da6c47dc698e62632943dfa25fcb79385bfd7
superj <15114901361@163.com>
2018-09-05 10:56:35 +0800

    parseXml:smile:

--- src/test/java/com/creatshare/answerapi/util/ParseXMLTest.java
+++ src/test/java/com/creatshare/answerapi/util/ParseXMLTest.java
@@ -10,6 +10,8 @@ public class ParseXMLTest {
     @Test
     public void parseXmlString() throws DocumentException {
 
+
+
         String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
                 +"<answers>"
                 +"  <answer>"
